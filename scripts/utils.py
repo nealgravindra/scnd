c6e8b3d83c313d7e917fa8124b35f171f90e0fe0
ngrrud <ngravindra@gmail.com>
2021-11-11 01:41:19 -0500

    hum redo rnavel added

--- scripts/utils.py
+++ scripts/utils.py
@@ -57,7 +57,9 @@ def ctype_plots(tdata, markersoi, plotfp=None, short_name=None, groupby='ctype')
 def phateumap(AnnData, 
               recalc_cluster=False, 
               recalc_pca=False, n_pcs=50,
-              recalc_knn=False, BBkNN=True, k=30, 
+              recalc_knn=False, BBkNN=True, k=30,
+              recalc_umap=True, recalc_phate=True,
+              decay=40,
               batch_key='batch',
               adata_out=None, t='auto',
               gamma=0):
@@ -82,20 +84,22 @@ def phateumap(AnnData,
             sc.pp.neighbors(AnnData, n_pcs=n_pcs, n_neighbors=k)
     if recalc_cluster:
         sc.tl.leiden(AnnData, resolution=3.0)
-    sc.tl.umap(AnnData)
+    if recalc_umap:
+        sc.tl.umap(AnnData)
 
-    # phate
-    G = gt.Graph(data=AnnData.uns['neighbors']['connectivities']+sparse.diags([1]*AnnData.shape[0],format='csr'),
-                 precomputed='adjacency',
-                 use_pygsp=True)
-    G.knn_max = None
-                                  
-    phate_op = phate.PHATE(knn_dist='precomputed',
-                           gamma=gamma,
-                           n_jobs=-1,
-                           t=t,
-                           random_state=42)
-    AnnData.obsm['X_phate']=phate_op.fit_transform(G.K)
+    if recalc_phate:
+        G = gt.Graph(data=AnnData.uns['neighbors']['connectivities']+sparse.diags([1]*AnnData.shape[0],format='csr'),
+                     precomputed='adjacency',
+                     use_pygsp=True)
+        G.knn_max = None
+
+        phate_op = phate.PHATE(knn_dist='precomputed',
+                               gamma=gamma,
+                               decay=decay,
+                               n_jobs=-1,
+                               t=t,
+                               random_state=42)
+        AnnData.obsm['X_phate']=phate_op.fit_transform(G.K)
     
     if adata_out is not None:
         scnddata.save_adata(AnnData, adata_out)
